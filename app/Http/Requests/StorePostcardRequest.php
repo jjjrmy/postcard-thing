a9bbcde05bba2c51c1d98f55c94d851a4460c2e0
Jeremy Holstein <jjjrmy@gmail.com>
2025-02-13 20:18:05 -0500

    fix file names

---
+++ app/Http/Requests/StorePostcardRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePostcardRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255', 'min:2'],
+            'address_1' => ['required', 'string', 'max:255', 'min:2'],
+            'address_2' => ['nullable', 'string', 'max:255'],
+            'city' => ['required', 'string', 'max:255'],
+            'state' => ['required', 'string', 'max:255'],
+            'zip' => ['required', 'string', 'max:10'],
+            'message' => ['required', 'string'],
+            'image_caption' => ['nullable', 'string', 'max:255'],
+            'image_url' => ['required', 'file', 'image', 'max:5120'], // 5MB max
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array<string, string>
+     */
+    public function attributes(): array
+    {
+        return [
+            'name' => 'Recipient Name',
+            'address_1' => 'Street Address',
+            'address_2' => 'Street Address Line 2',
+            'city' => 'City',
+            'state' => 'State',
+            'zip' => 'ZIP Code',
+            'message' => 'Message',
+            'image_caption' => 'Image Caption',
+            'image_url' => 'Image',
+        ];
+    }
+}
